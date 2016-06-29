36d5397e1ee4992eb20b420a962b95e3330ff226
Maurilio Atila <cabelotaina@gmail.com>
2016-06-29 16:58:23 -0300

    all file

---
+++ functions.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Sage includes
+ *
+ * The $sage_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/sage/pull/1042
+ */
+$sage_includes = [
+  'lib/assets.php',    // Scripts and stylesheets
+  'lib/extras.php',    // Custom functions
+  'lib/setup.php',     // Theme setup
+  'lib/titles.php',    // Page titles
+  'lib/wrapper.php',   // Theme wrapper class
+  'lib/customizer.php' // Theme customizer
+];
+
+foreach ($sage_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+  }
+
+  require_once $filepath;
+}
+unset($file, $filepath);
+
+
+/*
+ * Revista Agriculturas e Campanha
+ * 
+ * Inclui os arquivos relacionados com estas duas áreas do site
+ */
+include( plugin_dir_path( __FILE__ ).'functions-revista.php' );
+include( plugin_dir_path( __FILE__ ).'functions-campanha.php' );
+
+/*
+ * Custom Taxonomies
+ * Criando as taxonomias personalizadas 'Temas de intervenção' / 'Programas'
+ */
+function build_taxonomies() {
+
+	// Temas de intervenção
+	  $labels = array(
+	    'name' 				=> 'Temas de intervenção',
+	    'singular_name'	 	=> 'Tema de intervenção',
+	    'search_items' 		=> 'Pesquisar temas',
+	    'all_items' 		=> 'Todos os temas',
+	    'parent_item' 		=> 'Tema pai',
+	    'parent_item_colon' => 'Tema pai: ',
+	    'edit_item' 		=> 'Editar tema', 
+	    'update_item' 		=> 'Atualizar tema',
+	    'add_new_item' 		=> 'Adicionar Novo Tema de Intervenção',
+	    'new_item_name' 	=> 'Novo tema',
+	    'menu_name' 		=> 'Temas de intervenção'
+	  ); 	
+	
+	  register_taxonomy( 'temas-de-intervencao', 'post', array(
+	    'hierarchical'		=> true,
+	    'labels' 			=> $labels,
+	    'show_ui' 			=> true,
+	  	'show_in_nav_menus' => false,
+	    'query_var' 		=> true,
+	  	'capabilities'      => array('edit_terms' => false,'manage_terms' => false),
+	    'rewrite' 			=> array( 'slug' => 'aspta-temas-de-intervencao' ),
+	  ));
+
+	  // Programas
+	  $labels = array(
+	    'name' 				=> 'Programas',
+	    'singular_name'	 	=> 'Programa',
+	    'search_items' 		=> 'Pesquisar programas',
+	    'all_items' 		=> 'Todos os programas',
+	    'parent_item' 		=> 'Programa pai',
+	    'parent_item_colon' => 'Programa pai: ',
+	    'edit_item' 		=> 'Editar programa', 
+	    'update_item' 		=> 'Atualizar programa',
+	    'add_new_item' 		=> 'Adicionar Novo Programa',
+	    'new_item_name' 	=> 'Novo programa',
+	    'menu_name' 		=> 'Programas'
+	  ); 	
+	
+	  register_taxonomy( 'programas', 'post', array(
+	    'hierarchical'		=> true,
+	    'labels' 			=> $labels,
+	    'show_ui' 			=> true,
+	  	'show_in_nav_menus' => false,
+	    'query_var' 		=> true,
+	  	'capabilities'      => array('edit_terms' => false,'manage_terms' => false),
+	    'rewrite' 			=> array( 'slug' => 'aspta-programas' ),
+	  ));
+}
+
+add_action( 'init', 'build_taxonomies', 0 );
---
+++ templates/library.php
@@ -0,0 +1,103 @@
+<?php 
+/* 
+Template Name: Biblioteca 
+*/
+?>
+
+<h1><?php the_title();?></h1>
+
+<div class="library-box">
+  <h4>Tipos de Materiais</h4>
+  
+  <?php 
+  $terms = get_terms( 'category', array( 'hide_empty' => false ) );
+  
+  foreach($terms as $term){
+    if ($term->slug !== "sem-categoria"){
+      echo "<input type='checkbox' name='term-" . $term->slug . "' value='" . $term->name . "' />";
+        echo "<label for='term-" . $term->slug . "'>" . $term->name . "</label>";
+    }
+  }
+  
+  ?>
+<div>
+<div class="library-box">
+  <h4>Temas de intervenção</h4>
+  <?php
+  
+  $terms = get_terms( 'temas-de-intervencao', array( 'hide_empty' => false ) );
+  
+  foreach($terms as $term){
+    echo "<label for='term-" . $term->slug . "'>" . $term->name . "</label>";
+    echo "<input type='checkbox' name='term-" . $term->slug . "' value='" . $term->name . "' />";
+  }
+  ?>
+</div>
+<div class="library-box">
+  <h4>Revista Agriculturas</h4>
+  <p>
+    <label>Titúlo de artigo</label>
+    <input type="text" name="article_title" />
+  </p>
+  <p>
+    <label>Autor</label>
+    <input type="text" name="article_author" />
+  </p>
+  <p>
+    <label>Edição</label>
+<?php
+// Create a new Query
+$args = array(
+         'post_type' => 'revista',
+         'fields' => 'ids',
+       'post_status' => 'publish',
+       's' => 'V',
+       'posts_per_page' => -1
+  );
+$the_query = new WP_Query( $args );
+ 
+// The Loop
+?>
+<select name ="edition">
+<?php
+while ( $the_query->have_posts() ):
+$the_query->the_post();
+$target = get_the_title();
+preg_match("/V\d{1,3},\sN\d{1,3}/", $target , $keyword);
+if (!empty($keyword)){
+?>
+  <option value="<?php echo $keyword[0]; ?>"><?php echo $keyword[0]; ?></option>
+<?php
+}
+
+endWhile;
+?>
+</select>
+<?php 
+// Restore original Query
+wp_reset_query();
+
+?>
+  </p>
+  <p>
+    <label>Ano</label>
+    <?php $years = range(date("Y"), 2011); ?>
+    <select name="year">
+     <?php foreach($years as $year){ ?>
+     <option value="<?php echo $year; ?>"><?php echo $year; ?></option>
+     <?php } ?>
+    </select>
+  </p>
+</div>
+<div class="library-box">
+  <h4>Programas</h4>
+  <?php
+  
+  $terms = get_terms( 'programas', array( 'hide_empty' => false ) );
+  
+  foreach($terms as $term){
+    echo "<label for='term-" . $term->slug . "'>" . $term->name . "</label>";
+    echo "<input type='checkbox' name='term-" . $term->slug . "' value='" . $term->name . "' />";
+  }
+?>
+</div>
