61579965dcb4af81b19d239a9f7488e87c642d6d
Maurilio Atila <cabelotaina@gmail.com>
2016-06-30 18:51:40 -0300

    fix header

--- templates/header.php
+++ templates/header.php
@@ -28,11 +28,10 @@
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar">teste</span>
+        <span class="icon-bar">teste</span>
+        <span class="icon-bar">teste</span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
