29088f4d38505ba4a957ede83efa625153da8fba
Maurilio Atila <cabelotaina@gmail.com>
2016-06-29 23:02:19 -0300

    initial dev for  customize logo

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,21 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <form role="search" method="get" class="searchform group" action="<?php echo home_url( '/' ); ?>">
+        <input type="search" class="search-field"
+        placeholder="Busca!"
+        value="<?php echo get_search_query() ?>" name="s"
+        title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+    </form>
+    <?php if ( get_theme_mod( 'aspta_sage_upload_logo' ) ) : ?>
+    <div class='site-logo'>
+      <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'aspta_sage_upload_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+    </div>
+<?php else : ?>
+    <hgroup>
+        <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+        <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
+    </hgroup>
+<?php endif; ?>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
