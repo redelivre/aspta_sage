013cd6be1fb608d89e207b780ea133390a901b91
Maurilio Atila <cabelotaina@gmail.com>
2016-06-30 23:04:33 -0300

    added links for blog em pratos limpos sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -3,41 +3,40 @@
 <h2>Revista Agriculturas</h2>
 <?php
 query_posts( array (
-  'post_type'      => 'revista',
-  'post_parent'    => 0,
-  'posts_per_page' => 1,
-  'paged'          => $paged
-));
+      'post_type'      => 'revista',
+      'post_parent'    => 0,
+      'posts_per_page' => 1,
+      'paged'          => $paged
+      ));
 if ( have_posts() ) : 
-while ( have_posts() ) : the_post()?>
-<a href="<?php echo get_permalink( $revista->ID ); ?> "><?php the_revista_thumbnail( 'large' ); ?></a>
-<?php
-endwhile;
-endif;
-$args = array(
-       'post_type' => 'post',
-       'fields' => 'ids',
-       'post_status' => 'publish',
-       'order' => 'DESC',
-       'orderby' => 'date',
-       'posts_per_page' => 6 
-  );
-$the_query = new WP_Query( $args );
+  while ( have_posts() ) : the_post()?>
+  <a href="<?php echo get_permalink( $revista->ID ); ?> "><?php the_revista_thumbnail( 'large' ); ?></a>
+  <?php
+  endwhile;
+  endif;
+  ?>
 
+  <h2>Blog em pratos Limpos</h2>
+<?php
+  $rss = fetch_feed('http://pratoslimpos.org.br/?feed=rss2');
+
+
+  if (!is_wp_error( $rss ) ) : 
+
+    $maxitems = $rss->get_item_quantity(6); 
+    $rss_items = $rss->get_items(0, $maxitems); 
+    endif;
 ?>
+<?php 
+if ($maxitems == 0) echo '<li>'.__("Não há itens no blog.").'</li>';
+else 
+foreach ( $rss_items as $item ) : ?>
+<a href='<?php echo esc_url( $item->get_permalink() ); ?>' title='<?php echo esc_html( $item->get_title() ); ?>'> <?php echo esc_html( $item->get_title() ); ?></a>
+<?php endforeach; ?>
 
-<h2>Blog em pratos Limpos</h2>
 
-<?php
-while ( $the_query->have_posts() ):
-$the_query->the_post();
-?> 
 
-<p><a href="<?php get_permalink( the_ID() ); ?>"><?php echo get_the_title(); ?></a></p>
 
-<?php
-endWhile;
-?>
 <h2> Consulte Também</h2>
 
 <img src="<?php bloginfo('stylesheet_directory'); ?>/images/sidebar_partiners/alizanza.png" alt="parceiro: Alianza por la Agroecologia"/>
