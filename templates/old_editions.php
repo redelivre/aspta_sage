9d4fa3140afb2971d7799b00ba6a4910439c6dd7
Maurilio Atila <cabelotaina@gmail.com>
2016-07-01 00:22:02 -0300

    added new template for old newspaper editions

---
+++ templates/old_editions.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Template Name: Edições Anteriores
+ */
+query_posts( array (
+      'post_type'   => 'revista',
+      'post_parent' => 0,
+      'posts_per_page'=> -1,
+      'paged'     => $paged
+      ));
+
+?>
+<h4>Edições anteriores</h4>
+<div id="revistas-anteriores">
+<?php
+if ( have_posts() ) :
+  while ( have_posts() ) : the_post()?>
+  <a href="<?php echo get_permalink( $revista->ID ); ?> "><?php the_revista_thumbnail( 'medium' ); ?></a>
+  <?php
+  endwhile;
+  endif;
+  ?>
+</div>
+
+
