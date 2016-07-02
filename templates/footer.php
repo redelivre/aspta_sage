dcb25f7dd5c7da9fa3c29568bae79e429875b563
Maurilio Atila <cabelotaina@gmail.com>
2016-07-01 22:53:11 -0300

    add row ands cols on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,7 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+    	<?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
   </div>
 </footer>
