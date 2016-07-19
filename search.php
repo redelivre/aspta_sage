e8ca56bbba8378c3758cc070d3388169c8d5e1ee
Maurilio Atila <cabelotaina@gmail.com>
2016-07-19 19:03:41 -0300

    update search page with needed fields

--- search.php
+++ search.php
@@ -8,7 +8,7 @@
 <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'search'); ?>
+  <?php get_template_part('templates/search', 'search'); ?>
 <?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
