6f2ed1af75bec1d8ff211df94dc72237013bab40
Rodinei Costa <rodineicosta@physistec.com.br>
2016-07-26 06:37:45 -0300

    Update and fix code
    Update templates files fixing Nav Walker Menu responsive, thumbnail images of the posts (in progress) and fixing paths of images and scripts.

--- templates/header.php
+++ templates/header.php
@@ -6,14 +6,14 @@
 		<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 		
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>">
-		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/styles/font-awesome.min.css">
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script src="<?php bloginfo('template_url'); ?>/js/modernizr.custom.js"></script>
+		<script src="<?php bloginfo('template_url'); ?>/assets/scripts/modernizr.custom.js"></script>
 		<?php wp_head(); ?>
 	</head>
 			
@@ -21,16 +21,16 @@
 		<header class="header">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-7 col-sm-7">
+					<div class="col-md-7 col-sm-7 search">
 						<div id="sb-search" class="sb-search hidden-xs">
 							<form>
-								<input class="sb-search-input" placeholder="Digite o que procura e pressione enter..." type="text" value="" name="s" id="search">
+								<input class="sb-search-input" placeholder="Digite o que procura e pressione enter..." type="text" value="" name="search" id="search">
 								<input class="sb-search-submit" type="submit" value="">
 								<span class="sb-icon-search" aria-hidden="true" data-icon="&#xf002;"></span>
 							</form>
 						</div>
 					</div>
-					<div class="col-md-5 col-sm-5 logo"><a href="<?php bloginfo('url'); ?>" title="AS-PTA"><img src="<?php echo get_theme_mod('aspta_logo') ? get_theme_mod('aspta_logo') : get_template_directory_uri(). "/images/logo_aspta.png"; ?>" class="img-responsive center-block"></a></div>
+					<div class="col-md-5 col-sm-5 logo"><a href="<?php bloginfo('url'); ?>" title="AS-PTA"><img src="<?php echo get_theme_mod('aspta_logo') ? get_theme_mod('aspta_logo') : get_template_directory_uri(). "/assets/images/logo_aspta.png"; ?>" class="img-responsive center-block"></a></div>
 				</div>
 			</div><!-- /.topo -->
 			
@@ -40,11 +40,28 @@
 						<nav class="navbar navbar-justified" role="navigation">
 						  <div class="container-fluid">
 						    <!-- Brand and toggle get grouped for better mobile display -->
-						      <?php
-						      if (has_nav_menu('primary_navigation')) :
-						        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
-						      endif;
-						      ?>
+						    <div class="navbar-header">
+						    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						    		<span class="sr-only">Toggle navigation</span>
+						    		<span class="icon-bar"></span>
+						    		<span class="icon-bar"></span>
+						    		<span class="icon-bar"></span>
+						    	</button>
+						    </div>
+
+						        <?php
+						            wp_nav_menu( array(
+						                'menu'              => 'primary_navigation',
+						                'theme_location'    => 'primary_navigation',
+						                'depth'             => 2,
+						                'container'         => 'div',
+						                'container_class'   => 'collapse navbar-collapse',
+						        		'container_id'      => 'bs-example-navbar-collapse-1',
+						                'menu_class'        => 'nav navbar-nav',
+						                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+						                'walker'            => new wp_bootstrap_navwalker())
+						            );
+						        ?>
 						    </div>
 						</nav>
 					</div>
