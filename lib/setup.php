2098e30f8f9325e0338c19a64c2493776bc254a6
Rodinei Costa <rodineicosta@physistec.com.br>
2016-07-26 22:36:10 -0300

    Add suport for thumbnails
    Add suport for thumbnails and specify sizes of images.

--- lib/setup.php
+++ lib/setup.php
@@ -8,46 +8,54 @@ use Roots\Sage\Assets;
  * Theme setup
  */
 function setup() {
-  // Enable features from Soil when plugin is activated
-  // https://roots.io/plugins/soil/
-  // comented because we dont use soils plugin
-  // add_theme_support('soil-clean-up');
-  // add_theme_support('soil-nav-walker');
-  // add_theme_support('soil-nice-search');
-  // add_theme_support('soil-jquery-cdn');
-  // add_theme_support('soil-relative-urls');
+	// Enable features from Soil when plugin is activated
+	// https://roots.io/plugins/soil/
+	// comented because we dont use soils plugin
+	// add_theme_support('soil-clean-up');
+	// add_theme_support('soil-nav-walker');
+	// add_theme_support('soil-nice-search');
+	// add_theme_support('soil-jquery-cdn');
+	// add_theme_support('soil-relative-urls');
 
-  // Make theme available for translation
-  // Community translations can be found at https://github.com/roots/sage-translations
-  load_theme_textdomain('aspta', get_template_directory() . '/lang');
+	// Make theme available for translation
+	// Community translations can be found at https://github.com/roots/sage-translations
+	load_theme_textdomain('aspta', get_template_directory() . '/lang');
 
-  // Enable plugins to manage the document title
-  // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
-  add_theme_support('title-tag');
+	// Enable plugins to manage the document title
+	// http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
+	add_theme_support('title-tag');
 
-  // Register wp_nav_menu() menus
-  // http://codex.wordpress.org/Function_Reference/register_nav_menus
-  register_nav_menus([
-    'primary_navigation' => __('Menu Principal', 'aspta')
-  ]);
+	// Register wp_nav_menu() menus
+	// http://codex.wordpress.org/Function_Reference/register_nav_menus
+	register_nav_menus([
+	'primary_navigation' => __('Menu Principal', 'aspta')
+	]);
 
-  // Enable post thumbnails
-  // http://codex.wordpress.org/Post_Thumbnails
-  // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
-  // http://codex.wordpress.org/Function_Reference/add_image_size
-  add_theme_support('post-thumbnails');
+	// Enable post thumbnails
+	// http://codex.wordpress.org/Post_Thumbnails
+	// http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+	// http://codex.wordpress.org/Function_Reference/add_image_size
+	add_theme_support('post-thumbnails');
 
-  // Enable post formats
-  // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+	// Enable post formats
+	// http://codex.wordpress.org/Post_Formats
+	add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
-  // Enable HTML5 markup support
-  // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+	// Enable HTML5 markup support
+	// http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
+	add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 
-  // Use main stylesheet for visual editor
-  // To add custom styles edit /assets/styles/layouts/_tinymce.scss
-  add_editor_style(Assets\asset_path('styles/main.css'));
+	// Use main stylesheet for visual editor
+	// To add custom styles edit /assets/styles/layouts/_tinymce.scss
+	add_editor_style(Assets\asset_path('styles/main.css'));
+
+	// Habilita suporte a thumbnails e define tamanhos.
+	add_theme_support('post-thumbnails');
+	set_post_thumbnail_size(640, '', true);
+	add_image_size('destaque', 360, 258, true);
+	add_image_size('medio', 200, 200, true);
+	add_image_size('pequeno', 150, 150, true);
+
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 
