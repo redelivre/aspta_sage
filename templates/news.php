81a274006351337ab343d3d40c15006f7aff3238
Maurilio Atila <cabelotaina@gmail.com>
2016-08-02 21:52:08 -0300

    remove prev next from pagination

--- templates/news.php
+++ templates/news.php
@@ -67,7 +67,8 @@ echo paginate_links( array(
 	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 	'format' => '?paged=%#%',
 	'current' => max( 1, get_query_var('paged') ),
-	'total' => $the_query->max_num_pages
+	'total' => $the_query->max_num_pages,
+        'prev_next' => false,
 ) );
 ?>
 </div>
