36ba8b672fa5a7006d74d86cd8f790da6ed4595e
Rodinei Costa <rodineicosta@physistec.com.br>
2016-08-04 04:49:32 -0300

    Update code
    Update code of base page.

--- base.php
+++ base.php
@@ -1,33 +1,27 @@
 <?php
-
 use Roots\Sage\Setup;
 use Roots\Sage\Wrapper;
-
 ?>
-
-<!doctype html>
-<html <?php language_attributes(); ?>>
-  <?php get_template_part('templates/head'); ?>
-  <body <?php body_class(); ?>>
-    <!--[if IE]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-      </div>
-    <![endif]-->
     <?php
       do_action('get_header');
       get_template_part('templates/header');
     ?>
     <div class="wrap" role="document">
-      <div class="content row">
-        <main class="main">
-          <?php include Wrapper\template_path(); ?>
+      <div class="content container">
+        <main class="main row">
+          <?php 
+          	include Wrapper\template_path();
+          	global $wp;
+			$type = key($wp->query_vars);
+		  ?>
+          <?php get_template_part('templates/' . $type); ?>
+          <?php 
+          	if (Setup\display_sidebar()) : ?>
+        	<aside class="sidebar col-md-4">
+        		<?php include Wrapper\sidebar_path(); ?>
+        	</aside><!-- /.sidebar -->
+        	<?php endif; ?>
         </main><!-- /.main -->
-        <?php if (Setup\display_sidebar()) : ?>
-          <aside class="sidebar">
-            <?php include Wrapper\sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
-        <?php endif; ?>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
