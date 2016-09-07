d47c147bd0738230ff3d9c1e5aa6de42256d7c73
Maurilio Atila <cabelotaina@gmail.com>
2016-09-06 21:09:22 -0300

    fix issuem flexslider

--- templates/footer.php
+++ templates/footer.php
@@ -25,7 +25,7 @@
 				</div>
 			</div>
 			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script-->
 			<!-- Include all compiled plugins (below), or include individual files as needed -->
 			<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 			<script src="<?php bloginfo('template_url'); ?>/js/classie.js"></script>
