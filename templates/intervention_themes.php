4319921ec69bc51a1445f22a33ec421fdedd38d2
Rodinei Costa <rodineicosta@physistec.com.br>
2016-12-13 20:39:17 -0200

    Update code links

--- templates/intervention_themes.php
+++ templates/intervention_themes.php
@@ -17,82 +17,108 @@
 
   <div class="row">
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/acesso-a-mercados.png"></div>
-        <div class="tema-titulo">Acesso a mercados</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/acesso-a-mercados/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/acesso-a-mercados.png"></div>
+          <div class="tema-titulo">Acesso a mercados</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrobiodiversidade.png"></div>
-        <div class="tema-titulo">Agrobio&shy;diversidade</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/agrobiodiversidade/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrobiodiversidade.png"></div>
+          <div class="tema-titulo">Agrobio&shy;diversidade</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrofloresta.png"></div>
-        <div class="tema-titulo">Agrofloresta</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/agrofloresta/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrofloresta.png"></div>
+          <div class="tema-titulo">Agrofloresta</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/construcao-do-conhecimento-agroecologico.png"></div>
-        <div class="tema-titulo">Construção do Conhecimento Agroecológico</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/construcao-do-conhecimento-agroecologico/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/construcao-do-conhecimento-agroecologico.png"></div>
+          <div class="tema-titulo">Construção do Conhecimento Agroecológico</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/criacao-animal.png"></div>
-        <div class="tema-titulo">Criação Animal</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/criacao-animal/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/criacao-animal.png"></div>
+          <div class="tema-titulo">Criação Animal</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/financiamento-da-transicao-agroecologica.png"></div>
-        <div class="tema-titulo">Financiamento da Transição Agroecológica</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/financiamento-da-transicao-agroecologica/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/financiamento-da-transicao-agroecologica.png"></div>
+          <div class="tema-titulo">Financiamento da Transição Agroecológica</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/infancia-e-juventude.png"></div>
-        <div class="tema-titulo">Infância e Juventude</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/infancia-e-juventude/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/infancia-e-juventude.png"></div>
+          <div class="tema-titulo">Infância e Juventude</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-da-agua.png"></div>
-        <div class="tema-titulo">Manejo da Água</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/manejo-da-agua/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-da-agua.png"></div>
+          <div class="tema-titulo">Manejo da Água</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-ecologico-dos-solos.png"></div>
-        <div class="tema-titulo">Manejo Ecológico<br />dos Solos</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/manejo-ecologico-dos-solos/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-ecologico-dos-solos.png"></div>
+          <div class="tema-titulo">Manejo Ecológico<br />dos Solos</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/monitoramento-da-transicao-agroecologica.png"></div>
-        <div class="tema-titulo">Monitoramento da Transição Agroecológica</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/monitoramento-da-transicao-agroecologica/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/monitoramento-da-transicao-agroecologica.png"></div>
+          <div class="tema-titulo">Monitoramento da Transição Agroecológica</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/mulheres-e-agroecologia.png"></div>
-        <div class="tema-titulo">Mulheres e Agroecologia</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/mulheres-e-agroecologia/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/mulheres-e-agroecologia.png"></div>
+          <div class="tema-titulo">Mulheres e Agroecologia</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box-verde">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/saude-e-alimentacao.png"></div>
-        <div class="tema-titulo">Saúde e Alimentação</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/saude-e-alimentacao/">
+        <div class="tema-box-verde">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/saude-e-alimentacao.png"></div>
+          <div class="tema-titulo">Saúde e Alimentação</div>
+        </div>
+      </a>
     </div>
     <div class="tema-card col-md-4">
-      <div class="tema-box">
-        <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/transgenicos.png"></div>
-        <div class="tema-titulo">Transgênicos</div>
-      </div>
+      <a href="<?php echo get_bloginfo(url); ?>/aspta-temas-de-intervencao/transgenicos/">
+        <div class="tema-box">
+          <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/transgenicos.png"></div>
+          <div class="tema-titulo">Transgênicos</div>
+        </div>
+      </a>
     </div>
   </div>
 </section>
