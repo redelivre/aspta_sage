1941398992f9c87f3f1e742d03c1eb8896207bf7
rodineicosta <rodineicosta@physistec.com.br>
2019-09-28 12:56:18 -0300

    Automatic switching of card background color on Intervention Themes page

--- templates/intervention_themes.php
+++ templates/intervention_themes.php
@@ -18,7 +18,7 @@
   <div class="row">
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/acesso-a-mercados/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/acesso-a-mercados.png"></div>
           <div class="tema-titulo">Acesso a mercados</div>
         </div>
@@ -26,7 +26,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/agrobiodiversidade/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrobiodiversidade.png"></div>
           <div class="tema-titulo">Agrobio&shy;diversidade</div>
         </div>
@@ -34,7 +34,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/agrofloresta/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/agrofloresta.png"></div>
           <div class="tema-titulo">Agrofloresta</div>
         </div>
@@ -42,7 +42,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/construcao-do-conhecimento-agroecologico/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/construcao-do-conhecimento-agroecologico.png"></div>
           <div class="tema-titulo">Construção do Conhecimento Agroecológico</div>
         </div>
@@ -50,7 +50,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/criacao-animal/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/criacao-animal.png"></div>
           <div class="tema-titulo">Criação Animal</div>
         </div>
@@ -58,7 +58,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/financiamento-da-transicao-agroecologica-2/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/financiamento-da-transicao-agroecologica.png"></div>
           <div class="tema-titulo">Financiamento da Transição Agroecológica</div>
         </div>
@@ -66,7 +66,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/infancia-e-juventude/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/infancia-e-juventude.png"></div>
           <div class="tema-titulo">Infância e Juventude</div>
         </div>
@@ -74,7 +74,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/infancia/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/infancia-e-juventude.png"></div>
           <div class="tema-titulo">Infância</div>
         </div>
@@ -82,7 +82,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/juventude/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-juventude.png"></div>
           <div class="tema-titulo">Juventude</div>
         </div>
@@ -90,7 +90,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/manejo-da-agua/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-da-agua.png"></div>
           <div class="tema-titulo">Manejo da Água</div>
         </div>
@@ -98,7 +98,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/manejo-ecologico-dos-solos/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/manejo-ecologico-dos-solos.png"></div>
           <div class="tema-titulo">Manejo Ecológico<br />dos Solos</div>
         </div>
@@ -106,7 +106,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/monitoramento-da-transicao-agroecologica-2/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/monitoramento-da-transicao-agroecologica.png"></div>
           <div class="tema-titulo">Monitoramento da Transição Agroecológica</div>
         </div>
@@ -114,7 +114,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/mulheres-relacoes-sociais-de-genero-e-de-geracao/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/mulheres-e-agroecologia.png"></div>
           <div class="tema-titulo">Mulheres e Agroecologia</div>
         </div>
@@ -122,7 +122,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/saude-e-alimentacao/">
-        <div class="tema-box-verde">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/saude-e-alimentacao.png"></div>
           <div class="tema-titulo">Saúde e Alimentação</div>
         </div>
@@ -130,7 +130,7 @@
     </div>
     <div class="tema-card col-md-4">
       <a href="<?php get_site_url(); ?>/temas-de-intervencao/transgenicos/">
-        <div class="tema-box-laranja">
+        <div class="tema-box">
           <div class="tema-icone"><img class="image-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/transgenicos.png"></div>
           <div class="tema-titulo">Transgênicos</div>
         </div>
