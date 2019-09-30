3fa0d3a0ee2e415eb578b82d1b2d28c21d1fa422
rodineicosta <rodineicosta@physistec.com.br>
2019-09-30 19:13:09 -0300

    Design thumbnail and description in Magazine page

--- templates/newspaper-header.php
+++ templates/newspaper-header.php
@@ -1,10 +1,11 @@
-<?php use Roots\Sage\Titles; ?>
+<?php use Roots\Sage\Titles;
 
-<?php if(!isset($issue)) $issue = get_term_by( 'id', get_newest_issuem_issue_id(), 'issuem_issue' );
-?>
+if (!isset($issue)) {
+  $issue = get_term_by('id', get_newest_issuem_issue_id(), 'issuem_issue');
+} ?>
 
 <div class="lista page-header newspaper-header">
   <div class="titulo-pagina newspaper-title"><h1><?= get_issuem_issue_title(); ?></h1></div>
-  <div class="descricao-newspaper"><?= $issue->description; ?></div>
-  <!--  <div class="newspaper-cover"><?= wp_get_attachment_image( get_issuem_issue_cover(), 'issuem-cover-image' ); ?> </div> --><!-- using widget for this -->
+  <div class="newspaper-cover"><?= wp_get_attachment_image(get_issuem_issue_cover(), 'issuem-cover-image'); ?></div><!-- using widget for this -->
+  <div class="newspaper-descricao"><?= $issue->description; ?></div>
 </div>
