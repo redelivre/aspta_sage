951c3c228c089a5cefb2f3118c0ac014d5d2bfc8
Jacson Passold <jacson@passold.com.br>
2019-12-09 15:14:19 -0300

    add missing session area

--- templates/program_contestado.php
+++ templates/program_contestado.php
@@ -7,22 +7,24 @@
  * @since AS-PTA 0.2
  */
 ?>
-<?php while (have_posts()) : the_post(); ?>
-<article class="pagina-conteudo" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail() ) : ?>
-	<div class="pagina-img">
-		<?php  the_post_thumbnail('destaque-pagina');  ?>
-	</div>
-	<?php endif; ?>
-	<div class="pagina-data">
-		<time class="updated" datetime="<?php get_post_time('c', true); ?>"><?php the_date(); ?></time>
-	</div>
-	<div class="pagina-categoria">
-		<?php the_category(); ?>
-	</div>
-	<h1 class="entry-title"><strong><?php the_title(); ?></strong></h1>
-    <div class="entry-content">
-    	<?php the_content(); ?>
-    </div>
-</article>
-<?php endwhile; ?>
+<section id="pagina">
+	<?php while (have_posts()) : the_post(); ?>
+	<article class="pagina-conteudo" <?php post_class(); ?>>
+		<?php if ( has_post_thumbnail() ) : ?>
+		<div class="pagina-img">
+			<?php  the_post_thumbnail('destaque-pagina');  ?>
+		</div>
+		<?php endif; ?>
+		<div class="pagina-data">
+			<time class="updated" datetime="<?php get_post_time('c', true); ?>"><?php the_date(); ?></time>
+		</div>
+		<div class="pagina-categoria">
+			<?php the_category(); ?>
+		</div>
+		<h1 class="entry-title"><strong><?php the_title(); ?></strong></h1>
+	    <div class="entry-content">
+	    	<?php the_content(); ?>
+	    </div>
+	</article>
+	<?php endwhile; ?>
+</section>
