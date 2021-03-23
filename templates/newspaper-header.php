2b3adf3857f4faec38af2fcae214a6f2fc491624
Jacson Passold <jacson@passold.com.br>
2021-03-23 16:26:17 -0300

    fix issue getter

--- templates/newspaper-header.php
+++ templates/newspaper-header.php
@@ -1,11 +1,22 @@
 <?php use Roots\Sage\Titles;
 
-if (!isset($issue)) {
-  $issue = get_term_by('id', get_newest_issuem_issue_id(), 'issuem_issue');
-} ?>
+$issue = false;
+if (isset($_REQUEST['issue']) ) {
+	$issue = get_term_by('slug', esc_attr($_REQUEST['issue']), 'issuem_issue');
+} else {
+	$issue = get_term_by( 'id', get_newest_issuem_issue_id(), 'issuem_issue' );
+}
+?>
 
 <div class="lista page-header newspaper-header">
-  <div class="titulo-pagina newspaper-title"><h1><?= get_issuem_issue_title(); ?></h1></div>
-  <div class="newspaper-cover"><?= wp_get_attachment_image(get_issuem_issue_cover(), 'issuem-cover-image'); ?></div><!-- using widget for this -->
+  <div class="titulo-pagina newspaper-title"><h1><?= get_issuem_issue_title($issue->term_id); ?></h1></div>
+  <?php
+  $issue_meta = get_option( 'issuem_issue_' . $issue->term_id . '_meta' );
+  if( is_array($issue_meta) && isset( $issue_meta['cover_image'] ) ):
+  ?>
+  	<div class="newspaper-cover"><?= wp_get_attachment_image(get_issuem_issue_cover($issue->term_id), 'issuem-cover-image'); ?></div><!-- using widget for this -->
+  <?php
+  endif;
+  ?>
   <div class="newspaper-descricao"><?= $issue->description; ?></div>
 </div>
